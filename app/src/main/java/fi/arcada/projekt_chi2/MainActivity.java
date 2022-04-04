2bf3f5e8af7c31db00ca8dc54705d0b471d430ee
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2022-04-05 00:30:00 +0300

    Del 1, INLÄMNING.

--- app/src/main/java/fi/arcada/projekt_chi2/MainActivity.java
+++ app/src/main/java/fi/arcada/projekt_chi2/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // Deklarera 4 Button-objekt
     Button btn1, btn2, btn3, btn4;
     // Deklarera 4 heltalsvariabler för knapparnas värden
-    int val1, val2, val3, val4;
+    double val1, val2, val3, val4;
+    // TextView
+    TextView textPercentage, textPercentage2, statsField;
+
 
 
     @Override
@@ -25,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
         btn3 = findViewById(R.id.button3);
         btn4 = findViewById(R.id.button4);
 
-
+        textPercentage = findViewById(R.id.textView3);
+        textPercentage2 = findViewById(R.id.textView4);
+        statsField = findViewById(R.id.textView5);
     }
 
     /**
@@ -38,45 +44,81 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) view;
 
         // Kontrollera vilken knapp som klickats, öka värde på rätt vaiabel
-        if (view.getId() == R.id.button1) val1++;
+
         if (view.getId() == R.id.button2) val2++;
         if (view.getId() == R.id.button3) val3++;
         if (view.getId() == R.id.button4) val4++;
+        if (view.getId() == R.id.button1) val1++;
 
         // Slutligen, kör metoden som ska räkna ut allt!
         calculate();
     }
 
-    /**
-     * Metod som uppdaterar layouten och räknar ut själva analysen.
-     */
-    public void calculate() {
-
-        // Uppdatera knapparna med de nuvarande värdena
+    public void buttonClear(View view) {
+        val1 = 0; val2 = 0; val3 = 0; val4 = 0;
         btn1.setText(String.valueOf(val1));
         btn2.setText(String.valueOf(val2));
         btn3.setText(String.valueOf(val3));
         btn4.setText(String.valueOf(val4));
 
-        // Mata in värdena i Chi-2-uträkningen och ta emot resultatet
-        // i en Double-variabel
-        double chi2 = Significance.chiSquared(val1, val2, val3, val4);
-
-        // Mata in chi2-resultatet i getP() och ta emot p-värdet
-        double pValue = Significance.getP(chi2);
-
-        /**
-         *  - Visa chi2 och pValue åt användaren på ett bra och tydligt sätt!
-         *
-         *  - Visa procentuella andelen jakande svar inom de olika grupperna.
-         *    T.ex. (val1 / (val1+val3) * 100) och (val2 / (val2+val4) * 100
-         *
-         *  - Analysera signifikansen genom att jämföra p-värdet
-         *    med signifikansnivån, visa reultatet åt användaren
-         *
-         */
-
+        textPercentage.setText("");
+        textPercentage2.setText("");
+        statsField.setText("");
     }
 
+    /**
+     * Metod som uppdaterar layouten och räknar ut själva analysen. €
+     */
+    public void calculate() {
+        try {
+            // Uppdatera knapparna med de nuvarande värdena
+            btn1.setText(String.valueOf(val1));
+            btn2.setText(String.valueOf(val2));
+            btn3.setText(String.valueOf(val3));
+            btn4.setText(String.valueOf(val4));
 
+            // Mata in värdena i Chi-2-uträkningen och ta emot resultatet
+            // i en Double-variabel
+            double chi2 = Significance.chiSquared(val1, val2, val3, val4);
+
+            // Mata in chi2-resultatet i getP() och ta emot p-värdet
+            double pValue = Significance.getP(chi2);
+
+            double prob = 100 - (pValue * 100);
+            if (prob < 50) {
+                prob = 0;
+            }
+
+            double sign = 1.0 - pValue;
+
+            double tot1 = val1 + val3;
+            double tot2 = val2 + val4;
+            double p1 = val1/tot1 * 100;
+            double p2 = val2/tot2 * 100;
+
+            String childP = String.format("Children: %.1f", p1);
+            String adultP = String.format("Adults: %.1f", p2);
+            String statsF = String.format("Chi-2 result: %.2f\nSignificance: %.2f\nP-value: %.3f\n\nConfidence level: %.1f",
+            chi2, sign, pValue, prob
+            );
+
+            textPercentage.setText(childP + "%");
+            textPercentage2.setText(adultP + "%");
+            statsField.setText(statsF);
+
+            /**
+             *  - Visa chi2 och pValue åt användaren på ett bra och tydligt sätt!
+             *
+             *  - Visa procentuella andelen jakande svar inom de olika grupperna.
+             *    T.ex. (val1 / (val1+val3) * 100) och (val2 / (val2+val4) * 100
+             *
+             *  - Analysera signifikansen genom att jämföra p-värdet
+             *    med signifikansnivån, visa reultatet åt användaren
+             *
+             */
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/fi/arcada/projekt_chi2/Significance.java
+++ app/src/main/java/fi/arcada/projekt_chi2/Significance.java
@@ -5,10 +5,25 @@ public class Significance {
     /**
      * Metod som räknar ut Chi-två på basis av fyra observerade värden (o1 - o4).
      */
-    public static double chiSquared(int o1, int o2, int o3, int o4) {
+    public static double chiSquared(double o1, double o2, double o3, double o4) {
 
         // heltalsvariabler tänkta att få de förväntade värdena
-        int e1, e2, e3, e4;
+        double e1, e2, e3, e4;
+        double chi = 0;
+
+        e1 = (o1 + o2) * (o1 + o3) / (o1 + o2 + o3 + o4);
+        double val1 = (o1-e1)*(o1-e1)/e1;
+
+        e2 = (o1 + o2) * (o2 + o4) / (o1 + o2 + o3 + o4);
+        double val2 = (o2-e2)*(o2-e2)/e2;
+
+        e3 = (o3 + o4) * (o1 + o3) / (o1 + o2 + o3 + o4);
+        double val3 = (o3-e3)*(o3-e3)/e3;
+
+        e4 = (o3 + o4) * (o2 + o4) / (o1 + o2 + o3 + o4);
+        double val4 = (o4-e4)*(o4-e4)/e4;
+
+        chi = val1 + val2 + val3 + val4;
 
         /**
          *  Implementera din egen Chi-två-uträkning här!
@@ -23,7 +38,7 @@ public class Significance {
          *
          * */
 
-        return 0.0;
+        return chi;
     }
 
 
@@ -49,7 +64,10 @@ public class Significance {
         if (chiResult >= 9.550) p = 0.002;
 
         return p;
+    }
 
+    public static double getPercentage (double val) {
+        return 0.0;
     }
 
 }
